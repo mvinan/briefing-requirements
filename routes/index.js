140acffcd714d2e48826b0b7bd20e91f01f3d895
Miguel Alejandro <mvinan@icloud.com>
2015-09-23 09:06:04 -0500

    change title and minor styles information

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Sesión de información' });
+  res.render('index', { title: 'Requerimientos de información' });
 });
 
 module.exports = router;
